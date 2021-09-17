b27a1c65f00a4bb74914b557ad8ad7dd3da54ee8
Natalya Mikhaylova <natalyaoazis82@gmail.com>
2021-09-17 18:30:42 +0300

    second task

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -5,28 +5,11 @@ import ru.netology.domain.*;
 public class PostManager {
     private Post[] posts;
 
-    public Post[] generateForFeed() {
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
         return null;
     }
-    private CommentsInfo[] comments;
 
-    public CommentsInfo[] generateForComments() {
-        return null;
-    }
-    private LikesInfo[] likes;
-
-    public LikesInfo[] generateForLikes() {
-        return null;
-    }
-    private Repost[] reposts;
-
-    public Repost[] generateForReposts() {
-        return null;
-    }
-    private ShowViews[] views;
-
-    public ShowViews[] generateForShowViews() {
-        return null;
+    public void delete(int ownerId, int postId) {
     }
 }
 
